38292d4b2ad7ac1defc52724aecd706e5216668e
gh <gh7777@mail.ru>
2022-03-17 00:23:27 +0300

    edit

--- src/main/java/reklama/hooks/Hooks.java
+++ src/main/java/reklama/hooks/Hooks.java
@@ -3,17 +3,20 @@ package reklama.hooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reklama.configReader.PropertiesReader;
 import reklama.pages.BasePage;
 
 import static reklama.configReader.ChromeConfig.getChromeDriver;
 
 public class Hooks {
-
+    private static final Logger LOG = LoggerFactory.getLogger(Hooks.class);
     private PropertiesReader confReader = new PropertiesReader();
 
     @Before
     public void test() {
+        LOG.info("<----------TEST START---------->");
         BasePage.driver = getChromeDriver();
         BasePage.driver.get(confReader.getValue("url"));
     }
